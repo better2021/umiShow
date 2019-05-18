a547983935c19f3edb9b99a79137240688303a83
feiyu <feiyu@tg10010.com>
2019-05-18 22:22:43 +0700

    'umi'

---
+++ p__blog__CodeBlock.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[3],{o6JS:function(e,a,n){"use strict";n.r(a);var u=n("q1tI"),r=n.n(u),t=(n("17x9"),n("uUdh"));class s extends u["PureComponent"]{render(){var e=this.props,a=e.language,n=e.value;return r.a.createElement(t["a"],{language:a},n)}}s.defaultProps={language:null},a["default"]=s}}]);
